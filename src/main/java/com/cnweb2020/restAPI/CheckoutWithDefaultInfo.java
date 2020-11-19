274b2ca4d58c0074211ed0fa3caec34e011188b1
quyenlnn <quyen.ln98@gmail.com>
2020-11-20 01:54:11 +0700

    all

--- src/main/java/com/cnweb2020/restAPI/CheckoutWithDefaultInfo.java
+++ src/main/java/com/cnweb2020/restAPI/CheckoutWithDefaultInfo.java
@@ -13,7 +13,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns = "api-checkout-with-default-info")
+@WebServlet(urlPatterns = "/api-checkout-with-default-info")
 public class CheckoutWithDefaultInfo extends HttpServlet {
 
     @Inject
